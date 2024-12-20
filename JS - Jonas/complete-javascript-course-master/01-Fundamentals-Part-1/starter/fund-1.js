8a340b90933f59282b5c12c4999dd5df62d38910
Nyasha 2.0 <brainy@Nyashas-MacBook-Air.local>
2024-12-20 12:15:38 -0700

    added js basics

---
+++ JS - Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/fund-1.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas')
+
+let firstName = "Mukakati";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Sales";
+let myCurrentJob = "Web Designer";
+
+log.console(myCurrentJob)
+
---
+++ JS basics - Mosh/js.js
@@ -0,0 +1,7 @@
+
+let name = 'Nyasha';
+console.log(name);
+
+//cannot be a reserved keyword
+//should be meaningful
+//cannot start with a number 
